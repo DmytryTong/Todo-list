66f54e518a64b144599e307a8effc081e7666996
Dmytry <donchykd@gmail.com>
2023-04-03 12:41:05 +0300

    Modified deadline field to unnecessary, fixed some mistakes in tag_list.html and task_list.html

--- todo_list/models.py
+++ todo_list/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     creation_time = models.DateTimeField()
-    deadline = models.DateTimeField(null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     complete = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
