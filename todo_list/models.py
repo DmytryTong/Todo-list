da83561bfc57deb7531d947a1b34a752f7c839dc
Dmytry <donchykd@gmail.com>
2023-04-03 15:12:29 +0300

    Adding default to a creation_time

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Tag(models.Model):
@@ -10,7 +11,7 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
-    creation_time = models.DateTimeField()
+    creation_time = models.DateTimeField(default=timezone.now)
     deadline = models.DateTimeField(blank=True, null=True)
     complete = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
